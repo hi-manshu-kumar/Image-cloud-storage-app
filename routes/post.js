bfe34e01ad72f73eaff9dec5810d3b776f9ce3c0
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2018-12-19 17:51:06 +0530

    fixed bugs and routes added

--- routes/post.js
+++ routes/post.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.status(200).send("hello world");
+    res.send("entered post url");
 });
 
 module.exports = router;
